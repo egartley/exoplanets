1d000d8950543d9cd61e42adf6c3144e248e3320
Evan Gartley <egartley@users.noreply.github.com>
2020-08-02 18:27:38 -0400

    Add basic search
    Can search by name, host star, and year discovered

--- web/script.js
+++ web/script.js
@@ -1,24 +1,63 @@
-LIMIT = 200
-MAX_RAD = 2
-MIN_RAD = 1
-MAX_VISUAL = 72
-MIN_VISUAL = 24
-MAX_ORBIT = 1000
-MIN_ORBIT = 1
-GOLDEN = 0.618033988749895
-LIGHTYEAR = 3.26156378
+const TEST_LIMIT = 20
+const MIN_RAD = 1
+const MAX_RAD = 2
+const MIN_VISUAL = 24
+const MAX_VISUAL = 72
+const MIN_ORBIT = 1
+const MAX_ORBIT = 1000
+// const GOLDEN = 0.618033988749895
+const PC_TO_LY = 3.26156378
+const EMPTY_VALUE = "EMPTY"
+const SEARCH_TYPES = ["pl_name", "pl_hostname", "pl_disc"]
 
-function get(value, exoplanet, values) {
-    return exoplanet[values.indexOf(value)]
+let VALUES = []
+let DATA = []
+
+/**
+ * Returns the specified data value of an exoplanet
+ *
+ * @param value The name/label of the value to retrieve
+ * @param exoplanet The exoplanet in which to get the value from
+ * @returns {*} The specified data value, or an empty string if not found
+ */
+function get(value, exoplanet) {
+    const i = VALUES.indexOf(value)
+    return i !== -1 ? exoplanet[i] : EMPTY_VALUE
 }
 
-function get_list_item_html(values, exoplanet) {
-    return "<div class=\"list-item\" id=\"" + get("rowid", exoplanet, values) + "\"><div class=\"left\"><div class=\"visual\"></div></div><div class=\"right\"><span class=\"title\">" + get("pl_name", exoplanet, values) + "</span><span class=\"year\"><span class=\"list-value-label\">Found in:</span> " + get("pl_disc", exoplanet, values) + "</span><span class=\"distance\"><span class=\"list-value-label\">Distance:</span> " + (get("st_dist", exoplanet, values) * LIGHTYEAR).toFixed(2) + " ly</span></div></div>"
+function getListItemDataHTML(label, value, prettyName) {
+    return "<span class=\"" + prettyName + "\"><span class=\"list-value-label\">" + label + ":</span> " + String(value) + "</span>"
 }
+
+/**
+ * Returns HTML for the given exoplanet's list item
+ *
+ * @param exoplanet The exoplanet to be represented by the list item
+ * @returns {string} HTML for the exoplanet's list item
+ */
+function getListItemHTML(exoplanet) {
+    const rowid = get("rowid", exoplanet)
+    const name = get("pl_name", exoplanet)
+    const year = get("pl_disc", exoplanet)
+    let distance = get("st_dist", exoplanet)
+    // distance is in parsecs, so convert to lightyears
+    distance *= PC_TO_LY
+    // truncate to two decimals
+    distance = distance.toFixed(2)
+    return "<div class=\"list-item\" id=\"" + rowid + "\"><div class=\"left\"><div class=\"visual\"></div></div><div class=\"right\"><span class=\"title\">" + name + "</span>" + getListItemDataHTML("Found in", year, "year") + getListItemDataHTML("Distance", distance + " ly", "distance") + "</div></div>"
+}
+
+// Credit: https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+/*function getRandomColor() {
+    let h = Math.random(), s = 0.5, v = 0.95;
+    h += GOLDEN
+    h %= 1
+    return hsvToRgb(h, s, v)
+}*/
 
 // Credit: https://stackoverflow.com/a/17243070/13954969
-function hsv_to_rgb(h, s, v) {
-    var r, g, b, i, f, p, q, t;
+function hsvToRgb(h, s, v) {
+    let r, g, b, i, f, p, q, t;
     i = Math.floor(h * 6);
     f = h * 6 - i;
     p = v * (1 - s);
@@ -64,14 +103,6 @@ function hsv_to_rgb(h, s, v) {
     return "rgb(" + Math.round(r * 255) + ", " + Math.round(g * 255) + ", " + Math.round(b * 255) + ")"
 }
 
-// Credit: https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
-function get_random_color() {
-    var h = Math.random(), s = 0.5, v = 0.95
-    h += GOLDEN
-    h %= 1
-    return hsv_to_rgb(h, s, v)
-}
-
 /**
  * Scales, or "converts", a number in one range into another
  *
@@ -93,13 +124,16 @@ function scaled(x, r1Min, r1Max, r2Min, r2Max) {
     return r2Min + (r2Max - r2Min) * (x - r1Min) / (r1Max - r1Min)
 }
 
-function set_visual(values, exoplanet) {
-    // color based on orbital period
-    // size based on radius
-    var rowid = get("rowid", exoplanet, values)
-    var radius = get("pl_rade", exoplanet, values)
-    var orbit = get("pl_orbper", exoplanet, values)
-    var visual = $("div.list-item#" + rowid + " > div.left > div.visual")
+/**
+ * Sets the visual representation for the given exoplanet in its list item. Color is determined by orbitial period and size is determined by the exoplanet's radius
+ *
+ * @param exoplanet The exoplanet to set the visual for
+ */
+function setVisual(exoplanet) {
+    let radius = get("pl_rade", exoplanet);
+    let orbit = get("pl_orbper", exoplanet);
+    const rowid = get("rowid", exoplanet);
+    const visual = $("div.list-item#" + rowid + " > div.left > div.visual");
 
     // determine color
     if (orbit > MAX_ORBIT) {
@@ -107,7 +141,7 @@ function set_visual(values, exoplanet) {
     } else if (orbit < MIN_ORBIT) {
         orbit = MIN_ORBIT
     }
-    var color = hsv_to_rgb(scaled(orbit, MIN_ORBIT, MAX_ORBIT, 0, 360), 0.7, 0.95)
+    const color = hsvToRgb(scaled(orbit, MIN_ORBIT, MAX_ORBIT, 0, 360), 0.7, 0.95);
 
     // determine size
     if (radius > MAX_RAD) {
@@ -115,39 +149,110 @@ function set_visual(values, exoplanet) {
     } else if (radius < MIN_RAD) {
         radius = MIN_RAD
     }
-    var size = Math.round(scaled(radius, MIN_RAD, MAX_RAD, MIN_VISUAL, MAX_VISUAL))
+    const size = Math.round(scaled(radius, MIN_RAD, MAX_RAD, MIN_VISUAL, MAX_VISUAL));
 
     visual.css("width", size + "px")
     visual.css("height", size + "px")
     visual.css("background-color", color)
 }
 
-function exoSearch(e) {
-    var key = e.keyCode || e.which
+/**
+ * Prepare the list container for displaying results
+ */
+function list_readyResults() {
+    $("div.list-container span.list-start").hide()
+    $("div.list-container span.list-none").hide()
+    $("div.list-container").removeClass("list-container-empty")
+    $("div.list-container > div.list-item").remove()
+}
+
+/**
+ * Sets the list to display that there weren't any results
+ */
+function list_noResults() {
+    $("div.list-container > div.list-item").remove()
+    $("div.list-container").addClass("list-container-empty")
+    $("div.list-container span.list-none").show()
+}
+
+function exoKeyUp(e) {
+    const key = e.keyCode || e.which;
     if (13 === key) {
-        // TODO: search and display results
+        executeQuery($("input.exo-search").val())
     }
 }
 
-function go_test(values, exoplanets) {
-    $("div.list-container span.list-start").hide()
-    $("div.list-container").removeClass("list-container-empty")
-    $.each(exoplanets, function (index, exoplanet) {
-        if (index < LIMIT) {
-            $("div.list-container").append(get_list_item_html(values, exoplanet))
-            set_visual(values, exoplanet)
+function executeQuery(query) {
+    // get search type, make sure it's valid
+    const searchType = $("select.exo-search-type").val()
+    let valid = false
+    $.each(SEARCH_TYPES, function (index, type) {
+        if (type === searchType) {
+            valid = true
+        }
+    })
+    if (!valid) {
+        alert("There was a problem with validating the search type! (\"" + searchType + "\")")
+        return
+    }
+    // validate the query
+    valid = false
+    const originalQuery = query
+    if (query.includes(" ")) {
+        query = query.replace(/\s/g, "")
+    }
+    if (query.includes("-")) {
+        query = query.replace(/-/g, "")
+    }
+    if (query.includes("+")) {
+        query = query.replace(/\+/g, "")
+    }
+    if (/^[a-z0-9]+$/i.test(query) === false) {
+        alert("Invalid query! (can only include letters, numbers, spaces or \"+\" and \"-\")")
+        return
+    }
+    // query and type are valid, actually do the search
+    exoSearch(originalQuery, searchType)
+}
+
+function exoSearch(query, type) {
+    let cleared = false
+    const list = $("div.list-container")
+    $.each(DATA, function (index, exoplanet) {
+        if (get(type, exoplanet).includes(query)) {
+            if (!cleared) {
+                list_readyResults()
+                cleared = true
+            }
+            list.append(getListItemHTML(exoplanet))
+            setVisual(exoplanet)
+        }
+    })
+    if (!cleared) {
+        list_noResults()
+    }
+}
+
+function go_test() {
+    list_readyResults()
+    $.each(DATA, function (index, exoplanet) {
+        if (index < TEST_LIMIT) {
+            $("div.list-container").append(getListItemHTML(exoplanet))
+            setVisual(exoplanet)
         }
     })
 }
 
-function go(values, exoplanets) {
+/*function go() {
     
-}
+}*/
 
 $(document).ready(function () {
     $.getJSON("values.json", function (values) {
         $.getJSON("data.json", function (data) {
-            go_test(values, data)
+            VALUES = values
+            DATA = data
+            go_test()
         })
     })
 })
